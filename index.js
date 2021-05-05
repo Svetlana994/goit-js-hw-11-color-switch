3410d3e925f1ef52a2c148125a999cf2529f982b
Svetlana994 <07svetlana94@gmail.com>
2021-05-05 22:40:21 +0300

    Переключатель цветов

---
+++ index.js
@@ -0,0 +1,37 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    btnStart: document.querySelector('[data-action="start"]'),
+    btnStop: document.querySelector('[data-action="stop"]'),
+    body: document.body
+}
+
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', onStartBtnClick);
+refs.btnStop.addEventListener('click', onStopBtnClick);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStartBtnClick() {
+    intervalId = setInterval(() => {
+        const randomColor = randomIntegerFromInterval(0, colors.length);
+        refs.body.style.backgroundColor = colors[randomColor];
+        refs.btnStart.setAttribute('disabled', true)
+       ;
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    refs.btnStart.removeAttribute('disabled')
+}
